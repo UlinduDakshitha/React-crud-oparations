c5cb8c0c56c606f82fba7df0bfe6e54b4b33f329
Bandara W.M.U.D. <166277247+UlinduDakshitha@users.noreply.github.com>
2025-12-30 16:42:05 +0530

    build one component

---
+++ src/StudentTable.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function StudentTable() {
+  return (
+    <div className='container'>
+        <h2>Student Table</h2>
+      
+    </div>
+  )
+}
+
+export default StudentTable
