f0c1377d7b50e10f9898cf435b77566205248f92
Bandara W.M.U.D. <166277247+UlinduDakshitha@users.noreply.github.com>
2025-12-30 17:18:23 +0530

    update

--- src/StudentCrud/StudentTable.jsx
+++ src/StudentCrud/StudentTable.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { styled } from '@mui/material/styles';
+
+const Div = styled('div')(({ theme }) => ({
+  ...theme.typography.button,
+  backgroundColor: (theme.vars || theme).palette.background.paper,
+  padding: theme.spacing(1),  
+}));
 
 function StudentTable() {
   return (
-    <div className='container'>
-        <h2>Student Table</h2> 
+    <div className='container' style={{display: 'flex', justifyContent: 'center', alignItems: 'center', }}>
+     <Div sx={{Display:"flex",alignItems:"center",justifyContent:"center",color:"Red",fontSize:"50px",fontWeight:'bold'}}>Student Table</Div>
       
     </div>
   )
